5ad2c339b20c326e1df89c2206bae96c0a7b5dff
790013438 <790013438@qq.com>
2017-10-27 19:46:08 +0800

    取一个webserver变量名
     It would be nice to give a meaningful name to arguments. This could be done easily be adding the @WSParam annotation in our web service implementation class, CourseManagementService.

--- src/main/java/snippets/jee/ws/soap/CourseManagementService.java
+++ src/main/java/snippets/jee/ws/soap/CourseManagementService.java
@@ -3,6 +3,7 @@ package snippets.jee.ws.soap;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
@@ -12,7 +13,7 @@ public class CourseManagementService {
         //Here courses could be fetched from database using, 
         //for example, JDBC or JDO. However, to keep this example 
         //simple, we will return hard coded list of courses
-        
+
         List<Course> courses = new ArrayList<Course>();
 
         courses.add(new Course(1, "Course-1", 4));
@@ -21,7 +22,7 @@ public class CourseManagementService {
         return courses;
     }
 
-    public Course getCourse (int courseId) {
+    public Course getCourse (@WebParam(name="courseId") int courseId) {
         //Here again, we could get course details from database using
         //JDBC or JDO. However, to keep this example 
         //simple, we will return hard coded course.
